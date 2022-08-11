2b8fb87cf0d44af4763f0ccf6622cac9659f152a
Vagif <67714963+vagif-cmyk@users.noreply.github.com>
2022-08-11 20:31:39 +0300

    added skeleton and react-router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Sort from "./components/Sort";
-import Categories from "./components/Categories";
-import PizzaBlock from "./components/PizzaBlock";
-
-const URL = "https://62f3e2e9b81dba4a013e2516.mockapi.io/items";
 
 function App() {
-
-  const [pizzas, setPizzas] = useState([]);
-
-  useEffect(() => {
-    fetch(URL)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-        setPizzas(res);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
-      <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {pizzas.map((pizza) => (
-              <PizzaBlock key={pizza.id} {...pizza} />
-            ))}
-          </div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
