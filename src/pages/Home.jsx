d28ca9f7d29803f9efd7515ec6f62f03bf1f0f56
Vagif <67714963+vagif-cmyk@users.noreply.github.com>
2022-08-20 15:07:45 +0300

    added pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Sort from "../components/Sort";
 import Categories from "../components/Categories";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import Pagination from "../components/Pagination";
 
 const Home = ({ searchValue }) => {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: "популярности",
     sortBy: "rating",
@@ -22,7 +24,7 @@ const Home = ({ searchValue }) => {
     categoryId > 0 ? `category=${categoryId}` : ""
   }&sortBy=${sortType.sortBy}&order=${sortAs}${
     searchValue ? `&search=${searchValue}` : ""
-  }`;
+  }&page=${currentPage}&limit=4`;
 
   useEffect(() => {
     setIsLoading(true);
@@ -35,7 +37,7 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   return (
     <>
@@ -50,6 +52,7 @@ const Home = ({ searchValue }) => {
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">{isLoading ? skeletons : items}</div>
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </>
   );
 };
